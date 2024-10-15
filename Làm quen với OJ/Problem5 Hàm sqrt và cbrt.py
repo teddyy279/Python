3214ce09cab9d7379364be503b384f3006e8d8fd
teddy279 <phambaokhue2792004@gmail.com>
2024-10-15 22:46:45 +0700

    Update Problem5 Hàm sqrt và cbrt.py

--- Làm quen với OJ/Problem5 Hàm sqrt và cbrt.py
+++ Làm quen với OJ/Problem5 Hàm sqrt và cbrt.py
@@ -1,7 +1,8 @@
 from math import*
 
-x = int(input())
-res1 = sqrt(x)
-res2 = pow(x, 1 / 3)
-print('%.2f' % res1)
-print('%.3f' % res2)
+x = float(input())
+res1 = floor(x)
+res2 = ceil(x)
+res3 = round(x)
+
+print(res1, res2, res3, sep = '\n')

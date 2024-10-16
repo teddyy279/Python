c056e6f015a1d1b44c8bf27b4ecd5c63e4f18a64
teddy279 <phambaokhue2792004@gmail.com>
2024-10-16 23:21:17 +0700

    Update Problem Luyện tập viết câu điều kiện.py

---
+++ Làm quen với OJ/Cấu trúc rẽ nhánh/Problem Luyện tập viết câu điều kiện.py
@@ -0,0 +1,32 @@
+n = int(input())
+if n % 2 == 0: 
+    print("YES")
+else: print("NO")
+if (n % 3 == 0) and (n % 5 == 0):
+    print("YES")
+else: print("NO")
+if (n % 3 == 0) and (n % 7 != 0):
+    print("YES")
+else: print("NO")
+if (n % 3 == 0) or (n % 7 == 0):
+    print("YES")
+else: print("NO")
+if (n >= 30) and (n <= 50):
+    print("YES")
+else: print("NO")
+if (n >= 30) and ((n % 2 == 0) or (n % 3 == 0) or (n % 5 == 0)):
+    print("YES")
+else: print("NO")
+r = n % 10
+if n >= 10 and n <= 99 and (r == 2 or r == 3 or r == 5 or r == 7):
+    print("YES")
+else: print("NO")
+if (n <= 100) and n % 23 == 0 :
+    print("YES")
+else: print("NO")
+if n < 10 and n > 20:
+    print("NO")
+else: print("YES")
+if r % 3 == 0:
+    print("YES")
+else: print("NO")

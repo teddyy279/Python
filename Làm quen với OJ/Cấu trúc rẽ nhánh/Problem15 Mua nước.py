1cc838e23d9e02877ddd8341d3ed38ef2df63488
teddy279 <phambaokhue2792004@gmail.com>
2024-10-17 12:37:47 +0700

    Create Problem15 Mua nước.py

---
+++ Làm quen với OJ/Cấu trúc rẽ nhánh/Problem15 Mua nước.py
@@ -0,0 +1,7 @@
+n, a, b = map(int, input())
+if(b / 2 >= a): # b / 2 là tính nước trên 1 l của b 
+    print(a * n)
+else:
+    if(n % 2 == 0):
+        print(n // 2 * b)
+    else: print((n - 1) // 2 * b + a)

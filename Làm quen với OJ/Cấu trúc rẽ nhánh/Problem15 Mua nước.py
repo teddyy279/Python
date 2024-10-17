a1e094824e533a34c14768ad1cc2805ed72f5986
teddy279 <phambaokhue2792004@gmail.com>
2024-10-17 12:47:24 +0700

    Update Problem15 Mua nước.py

--- Làm quen với OJ/Cấu trúc rẽ nhánh/Problem15 Mua nước.py
+++ Làm quen với OJ/Cấu trúc rẽ nhánh/Problem15 Mua nước.py
@@ -5,3 +5,27 @@ else:
     if(n % 2 == 0):
         print(n // 2 * b)
     else: print((n - 1) // 2 * b + a)
+
+"""
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, a, b;
+    cin >> n >> a >> b;
+    vector<int> F(n + 1, INT_MAX);
+    F[0] = 0; 
+    for (int i = 1; i <= n; i++) {
+        if (i >= 1) {
+            F[i] = min(F[i], F[i - 1] + a);
+        }
+        if (i >= 2) {
+            F[i] = min(F[i], F[i - 2] + b);
+        }
+    }
+    cout << F[n];
+    return 0;
+}
+"""

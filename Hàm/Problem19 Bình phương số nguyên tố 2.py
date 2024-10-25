036b82fc002faf74f2326f2a78aa8bf3fbfc7001
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-10-25 15:40:50 +0700

    Create Problem19 Bình phương số nguyên tố 2.py

---
+++ Hàm/Problem19 Bình phương số nguyên tố 2.py
@@ -0,0 +1,18 @@
+from math import*
+
+def check(n):
+    for i in range(2, isqrt(n) + 1):
+        if n % i == 0:
+            cnt = 0
+            while(n % i == 0):
+                cnt += 1
+                n //= i
+            if(cnt >= 2): return True
+    return False
+
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    if a > b:
+        a, b = b, a
+    for i in range(a, b + 1):
+        if(check(i)): print(i, end = ' ')

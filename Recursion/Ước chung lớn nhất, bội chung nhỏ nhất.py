0c1e3f3966af6c8b171b98705901baf131afd966
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-10-28 15:03:28 +0700

    Create Ước chung lớn nhất, bội chung nhỏ nhất.py

---
+++ Recursion/Ước chung lớn nhất, bội chung nhỏ nhất.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    if(b == 0): return a
+    return gcd(b, a % b)
+
+def lcm(a, b):
+    return a * b // gcd(a, b) 
+
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    print(gcd(a, b), end = ' ')
+    print(lcm(a, b))
+

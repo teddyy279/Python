8eb666997b9f2dc2187dc541398a283c35b5b9ad
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-11 01:51:49 +0700

    Update Problem 35 Xếp trẻ.py

--- Sắp xếp - Tìm kiếm/Problem 35 Xếp trẻ.py
+++ Sắp xếp - Tìm kiếm/Problem 35 Xếp trẻ.py
@@ -1,15 +1,17 @@
-def solve(a, pos, x, cnt):
-    if pos == len(a):
-        return cnt
-    if a[pos] + a[pos - 1] < x:
-        cnt = solve(a, pos + 1, x, cnt)
-    else:
-        cnt += 1
-        cnt = solve(a, pos + 1, x, cnt)
-    return cnt
+def min_gondolas(n, x, weights):
+    weights.sort()
+    i, j = 0, n - 1
+    gondolas = 0
+
+    while i <= j:
+        if weights[i] + weights[j] <= x:
+            i += 1
+        j -= 1
+        gondolas += 1
+
+    return gondolas
 
 if __name__ == '__main__':
     n, x = map(int, input().split())
-    a = list(map(int, input().split()))
-    a.sort()
-    print(solve(a, 1, x, 1))
+    weights = list(map(int, input().split()))
+    print(min_gondolas(n, x, weights))

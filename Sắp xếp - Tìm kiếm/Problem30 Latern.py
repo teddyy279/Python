90f7669cee51b08a31cb80b73c4aa21a183d504b
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-11 00:54:45 +0700

    Update Problem30 Latern.py

--- Sắp xếp - Tìm kiếm/Problem30 Latern.py
+++ Sắp xếp - Tìm kiếm/Problem30 Latern.py
@@ -1,7 +1,8 @@
 if __name__ == '__main__':
     n, l = map(int, input().split())
     a = list(map(int, input().split()))
-    max_distance = -1
+    a.sort()
+    max_distance = (a[0] - 0, l - a[-1]) 
     for i in range(1, len(a)):
         if (a[i] - a[i - 1]) > max_distance:
             max_distance = a[i] - a[i - 1]

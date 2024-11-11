9f02e94cf31d7b81f8a96f5a9d7856cd5a6a027f
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-12 01:06:35 +0700

    Create Liệt kê và đếm.py

---
+++ Set, Dictionary/Liệt kê và đếm.py
@@ -0,0 +1,28 @@
+from math import*
+from sys import stdin
+
+def check(n):
+    r = n % 10
+    n //= 10
+    while n != 0:
+        if r < n % 10:
+            return False
+        r = n % 10
+        n //= 10
+    return True
+
+if __name__ == '__main__':
+    d = dict({})
+    for line in stdin:
+        a = list(map(int, input().split()))
+        for x in a:
+            if check(x):
+                if x in d:
+                    d[x] += 1
+                else:
+                    d[x] = 1
+    d = list(d.items())
+    d.sort(key = lambda x : (-x[1], x[0]))
+    for x, y in d:
+        print(x, y)
+

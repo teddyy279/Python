5a3a71d28076b0de143b6520c7de75c193511df5
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-16 00:52:17 +0700

    Create Problem15 Kiểm tra xâu con.py

---
+++ String/Problem15 Kiểm tra xâu con.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    s = input()
+    t = input()
+    if t in s:
+        print("YES")
+    else:
+        print("NO")
+
+
+"""
+if t.find(s):
+    print("YES")
+else:
+    print("NO")
+"""

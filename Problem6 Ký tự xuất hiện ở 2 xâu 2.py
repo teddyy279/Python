fccba062ff06c2e1ddb4968f30f165a867ceb447
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-15 22:40:28 +0700

    Delete Problem6 Ký tự xuất hiện ở 2 xâu 2.py

--- Problem6 Ký tự xuất hiện ở 2 xâu 2.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    s1 = input()
-    s2 = input()
-    set_S1 = set(s1)
-    set_S2 = set(s2)
-    unique_S1 = set_S1.difference(set_S2)
-    unique_S2 = set_S2.difference(set_S1)
-    print(''.join(sorted(unique_S1)))
-    print(''.join(sorted(unique_S2)))

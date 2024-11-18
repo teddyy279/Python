b80e1965f6b68c0106f57fa7ea56157a5592861a
teddyy279 <151600961+teddyy279@users.noreply.github.com>
2024-11-19 00:58:38 +0700

    Update Problem21 Tập từ chung của 2 xâu.py

--- String/Problem21 Tập từ chung của 2 xâu.py
+++ String/Problem21 Tập từ chung của 2 xâu.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     s = input()
     t = input()
     a = set(s.lower().split())
-    b = set(t.lower().split())
+    b = set(t.lower().split()) # phải dùng lower() trước khi biến thành set tại set không thay đổi được các phần tử của nó 
     for x in sorted(a):
         if x in b:
             print(x, end = ' ')
